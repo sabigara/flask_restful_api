dbc321de33b656d381f3cffb07c41f86bdd33c3e
Isami Yuma <lemonburst1958@gmail.com>
2018-11-05 15:20:10 +0900

    Modified configurations

--- config.py
+++ config.py
@@ -6,12 +6,11 @@ load_dotenv(os.path.join(basedir, '.flaskenv'))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'C5_h63b9p@FC6eFN'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secretkey'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    OAUTH2_REFRESH_TOKEN_GENERATOR = False
     OAUTH2_TOKEN_EXPIRES_IN = {
         'password': 15552000,
     }
